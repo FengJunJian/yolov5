e95c15f04025b7607a2fa7f39cd9b0e4bbab90fb
FJJ <fengjunjianx@qq.com>
2022-01-21 16:27:25 +0800

    bat for windows system

--- data/contine_train.sh
+++
@@ -1,3 +0,0 @@
-python train.py --img 640 --batch 16 --epochs 60 --data data/ship.yaml --weights weight/yolov5s.pt --cfg models/yolov5s.yaml
-python train.py --img 640 --batch 16 --epochs 60 --data data/ship.yaml --weights weight/yolov5m.pt --cfg models/yolov5m.yaml
-python train.py --img 640 --batch 16 --epochs 200 --data data/ship.yaml --weights weight/yolov5l.pt --cfg models/yolov5l.yaml
